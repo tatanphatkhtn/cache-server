5b80fb468134d546f60cfe9d352372c98f8aee64
hxtruong <truong@codelynx.io>
2018-10-18 11:31:20 +0700

    fix unit test

--- unitTestServer.py
+++ unitTestServer.py
@@ -21,8 +21,9 @@
 import json
 import requests
 
-SERVER_URL = "https://prod.take247.co.il/graphql"
-JSON_FILE_PATH = "cacheServer.json"
+SERVER_URL = "http://takearea.me:4002/"
+# JSON_FILE_PATH = "cacheServer.json"
+JSON_FILE_PATH = "/media/hxtruong/Data/CodeLynX/graphql-cache-server/ExtractLogs/result.json"
 
 # def run_query(query, variables, headers): # A simple function to use requests.post to make the API call. Note the json= section.
 #     request = requests.post(SERVER_URL, json={'query': query, 'variables': variables}, headers=headers)
@@ -31,12 +32,19 @@ JSON_FILE_PATH = "cacheServer.json"
 #     else:
 #         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
-    request = requests.post(SERVER_URL, json={'query': query})
+def run_query(query, variables): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post(SERVER_URL, json={'query': query, 'variables': variables})
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+# def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+#     request = requests.post(SERVER_URL, json={'query': query})
+#     if request.status_code == 200:
+#         return request.json()
+#     else:
+#         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 def load_json(file_path):
     # get json from file
@@ -45,30 +53,45 @@ def load_json(file_path):
     return data;
 
 def compareTest(expectedResult, requestResult):
+    print( expectedResult.__str__())
+    print(requestResult.__str__())
     return expectedResult.__str__() == requestResult.__str__()
 
 def displayDiff(expectedResult, requestResult):
     print("##### Expected result #####\n")
     print(json.dumps(expectedResult, indent=3))
-    print("\n##### Request result (TRUE) #####\n")
+    print("\n##### Request result #####\n")
     print(json.dumps(requestResult, indent=3))
     print('\n')
 
 def main():
-    qnaSeries = load_json(JSON_FILE_PATH)
-    #print(qnaSeries)
-
+    # qnaSeries = json.loads(JSON_FILE_PATH)
+    with open(JSON_FILE_PATH) as f:
+        qnaSeries = json.load(f)
+  
     passTest = 0;
-    for index, item in enumerate(qnaSeries):
-        #raise Exception(f"{item}")
-        query, expectedResult = item["query"], item["expectedResult"];
-        #print(f"index {index}\n query: {query} \n result: {expectedResult}")
-        requestResult = run_query(query)
-        if compareTest(expectedResult, requestResult):
-            passTest += 1
-        else:
-            print(f"The number {index + 1} test is FAILED!")
-            displayDiff(expectedResult, requestResult)
+    for index, item in enumerate(qnaSeries):      
+        # raise Exception(f"{item}")
+      
+        if index == 3:
+            log = json.loads(item["logs"])
+            # print(log)
+            queryInfo = json.loads(log["query"])
+            expectedResult = json.loads(log["result"])
+            query = queryInfo["query"]
+            variables = queryInfo["variables"]
+            typeQuery = log["type"]
+            timeQuery = log["time"]
+          
+            # print(f"Query: {query}\nResult: {expectedResult}")
+      
+            requestResult = run_query(query, variables)
+            if compareTest(expectedResult, requestResult):
+                passTest += 1
+            else:
+                print(f"The number {index + 1} test is FAILED!")
+                print("The Query: {0}\nThe variables: {1}".format(query, variables))
+                displayDiff(expectedResult, requestResult)
 
     
 
